28e29ecfbb56e7bfd0cb6a8630c0edaa9fc14618
John Tyree <johntyree@gmail.com>
2015-09-02 11:47:35 -0500

    We aren't changing the number of frames

--- tabs/static/js/MapView.js
+++ tabs/static/js/MapView.js
@@ -189,8 +189,6 @@ MapView = (function($, L, Models, Config) {
         var options = {datasource: self.datasource};
         API.withFrameTimestamps(options, function(data) {
             self.timestamps = data.timestamps;
-            // Reset the number of frames
-            self.nFrames = self.timestamps.length;
             // Choose most recent window
             self.frameOffset = self.timestamps.length - self.nFrames;
             if (self.tabsControl) {
