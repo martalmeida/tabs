ad9fb42ff170b90af171efa59d1066d51a8521ab
Kelsey Jordahl <kjordahl@enthought.com>
2015-10-06 23:11:47 -0500

    BUG: Use URL paths on deployed server

--- tabs/static/js/Config.js
+++ tabs/static/js/Config.js
@@ -2,11 +2,11 @@
 var Config = {
 
     // API
-    velocityGridLocationsURL: '/data/thredds/velocity/grid',
-    velocityFrameURL: '/data/thredds/velocity/step/',
-    saltFrameURL: '/data/thredds/salt/step/',
-    domainURL: '/data/prefetched/domain',
-    timestampURL: '/data/thredds/timestamps',
+    velocityGridLocationsURL: '/tabs/data/thredds/velocity/grid',
+    velocityFrameURL: '/tabs/data/thredds/velocity/step/',
+    saltFrameURL: '/tabs/data/thredds/salt/step/',
+    domainURL: '/tabs/data/prefetched/domain',
+    timestampURL: '/tabs/data/thredds/timestamps',
     tileLayerURL: 'https://{s}.tiles.mapbox.com/v4/tabs-enthought.j3nibphe/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidGFicy1lbnRob3VnaHQiLCJhIjoiX0RNTzlmbyJ9.gio9AiDol4CT13yJ2GyRHw',
 
     // Vector style
